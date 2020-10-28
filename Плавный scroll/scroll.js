09eec0d8673a689ae9217973569cda69fcd043d3
krasilovamaria <43666809+krasilovamaria@users.noreply.github.com>
2020-10-28 13:02:32 +0700

    Add files via upload

--- Плавный scroll/scroll.js
+++ Плавный scroll/scroll.js
@@ -1,1 +1,31 @@
+'use strict';
+// И в html пишем такой текст:
+// <a class="promo__scroll" href="#advantages" aria-label="scroll">Скролл вниз</a>
+// <section class="advantages" id="advantages">
 
+// плавный скролл
+var linkNav = document.querySelectorAll('[href^="#"]');
+var V = 0.5;
+for (var i = 0; i < linkNav.length; i++) {
+  linkNav[i].addEventListener('click', function (e) {
+    e.preventDefault();
+    var w = window.pageYOffset;
+    var hash = e.currentTarget.href.replace(/[^#]*(.*)/, '$1');
+    var t = document.querySelector(hash).getBoundingClientRect().top;
+    var start = null;
+    requestAnimationFrame(step);
+    function step(time) {
+      if (start === null) {
+        start = time;
+      }
+      var progress = time - start;
+      var r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+      window.scrollTo(0, r);
+      if (r !== w + t) {
+        requestAnimationFrame(step);
+      } else {
+        location.hash = hash;
+      }
+    }
+  }, false);
+}
