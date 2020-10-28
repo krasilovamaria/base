d924e98c9758ed2db9a793686c95604e687da7ee
krasilovamaria <43666809+krasilovamaria@users.noreply.github.com>
2020-10-28 13:17:37 +0700

    Add files via upload

--- localStorage/localstorage.js
+++ localStorage/localstorage.js
@@ -1,1 +1,14 @@
+'use strict';
 
+// localStorage - сохраняет данные в локальном хранилище 
+// (это данные, которые хранятся бессрочно и будут доступны даже после перезагрузки браузера)
+
+var username = popup.querySelector('[name=username]');
+var phone = popup.querySelector('[name=phone]');
+var popupField = popup.querySelector('[name=popup-field]');
+
+popup.addEventListener('submit', function () {
+  localStorage.setItem('username', username.value);
+  localStorage.setItem('phone', phone.value);
+  localStorage.setItem('popupField', popupField.value);
+});
