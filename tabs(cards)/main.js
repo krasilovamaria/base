d57e162ee34f12a824dd32ab04bfef20c36b5d4b
krasilovamaria <43666809+krasilovamaria@users.noreply.github.com>
2020-11-25 18:31:13 +0700

    Add files via upload

--- tabs(cards)/main.js
+++ tabs(cards)/main.js
@@ -1,1 +1,17 @@
+'use strict'
 
+var tabBtn = document.querySelectorAll('button');
+var tabContent = document.querySelectorAll('.tab-content');
+
+Object.keys(tabBtn).forEach(function (index) {
+  tabBtn[index].addEventListener('click', function (ev) {
+
+    for(var i = 0; i < tabBtn.length; i++){
+      tabBtn[i].classList.remove('active-btn');
+      tabContent[i].classList.remove('active-content');
+    }
+
+    ev.target.classList.add('active-btn');
+    tabContent[index].classList.add('active-content');
+  });
+});
